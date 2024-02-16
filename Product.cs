2c5529bb269ffb531a297ac91e097da084940e9e
Radhika <radhikasn06@gmail.com>
2024-02-16 15:20:03 +0530

    FirstCommit

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBulidAssignment3
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string? PName { get; set; }
+        public string? PBrand { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using AutoBulidAssignment3;
+SortedList<int, Product> productList = new SortedList<int, Product>();
+productList.Add(3, new Product { PId = 3, PName = "Chocs", PBrand = "DairyMilk", ManufacturingDate = DateTime.Now,
+    ExpiryDate = DateTime.Now.AddMonths(6) });
+//productList.Add(1, new Product { PId = 1, PName = "Biscuits", PBrand = "ParleG", ManufacturingDate = DateTime.Now,
+//ExpiryDate = DateTime.Now.AddMonths(12) });
+//productList.Add(2, new Product { PId = 2, PName = "Shampoo", PBrand = "sunsilk", ManufacturingDate = DateTime.Now,
+//ExpiryDate = DateTime.Now.AddMonths(9) });
+//productList.Add(3, new Product { PId = 3, PName = "Chocs", PBrand = "DairyMilk", ManufacturingDate = DateTime.Now,
+//ExpiryDate = DateTime.Now.AddMonths(6) });
+// Displaying the sorted list
+foreach (var product in productList)
+{
+    Console.WriteLine($"Key: {product.Key}, Product Name: {product.Value.PName}, Brand: {product.Value.PBrand}");
+}
+    
+
+
+
